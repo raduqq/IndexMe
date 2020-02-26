7ab9d34f44b837bcf05678165c8e1a5f9db36f04
Radu Minea <55953468+raduqq@users.noreply.github.com>
2020-02-26 10:21:19 +0200

    Lots of things left to do

--- IndexMe.c
+++ IndexMe.c
@@ -9,13 +9,17 @@
 #define MAX_LEN 50
 #define MAX_DOCS 100
 
-void clean_string(char s[]) {
-  char *p = strchr(s, '\n');
-  *p = 0;
+bool is_letter(char c) {
+  return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
-bool solve_brackets(char buffer[], int *no_brackets, int *i, bool *result,
-                    bool *NOT, int i_doc, int *j_term, char *terms[], char *index[]) {
+void clean_string(char s[]) {
+  char *p = strchr(s, '\n');
+  if (p != NULL) *p = 0;
+}
+
+bool solve_brackets(char buffer[], int *no_brackets, unsigned int *i, bool *result,
+                    bool *NOT, int i_doc, int *j_term, char terms[MAX_TERMS][MAX_LEN], bool **index) {
   // Backstop
   if (*no_brackets == 0) {
     if (*NOT == 1) {
@@ -24,49 +28,56 @@ bool solve_brackets(char buffer[], int *no_brackets, int *i, bool *result,
     return *result;
   }
 
-    if (buffer[(*i)++] == ' ') {
-        *(j_term)++;
-    }
-
     while ((buffer[*i] > 'a' && buffer[*i] < 'z') ||
                (buffer[*i] > 'A' && buffer[*i] < 'Z')) {
-        *i++;
+        *i += 1;
     }
 
-  if (buffer[(*i)++] == '!') {
+    if (buffer[*i] == ' ') {
+        *j_term += 1;
+        *i += 1;
+    }
+
+  if (buffer[*i] == '!') {
     *NOT = 1;
+    *i += 1;
   }
 
   // Finding another parantheses
-  if (buffer[(*i)++] == '(') {
-    (*no_brackets)++;
+  if (buffer[*i] == '(') {
+    *i += 1;
+    *no_brackets += 1;
     *result = solve_brackets(buffer, no_brackets, i, result, NOT, i_doc, j_term,
                              terms, index);
   }
 
-
     // Logical interpretation
     if (buffer[*i] == '&' && buffer[*i + 1] == '&') {
         *result &= index[*i][*j_term];
+        if(NOT == 1) *result = !(*result);
     }
 
     if (buffer[*i] == '|' && buffer[*i + 1] == '|') {
         *result |= index[*i][*j_term];
+        if(NOT == 1) *result = !(*result);
     }
         
     *i += 2;
 
   // Going down the stiva
-  if (buffer[(*i)++] == ')') {
-    (*no_brackets)--;
+  if (buffer[*i] == ')') {
+    *i += 1;
+    *no_brackets -= 1;
+    // solve_brackets(buffer, no_brackets, i, result, NOT, i_doc, j_term, terms, index);
   }
+
 }
 
 int main() {
   printf("Welcome to IndexMe, your very first search engine!\n");
   int no_terms;
   char terms[MAX_TERMS][MAX_LEN], buffer[MAX_LINE], buffer_aux[MAX_LINE], *p,
-      delim[] = "()! ", blanks[] = " ";
+      delim[] = "&()!| ", blanks[] = " ";
 
   // Identificarea documentelor in care se va realiza query-ul
   printf("Enter the docs to search through in the following format - \"doc1 "
@@ -95,6 +106,8 @@ int main() {
     if (strcasecmp(buffer, "q\n") == 0) {
       exit(0);
     }
+
+    clean_string(buffer);
     no_terms = 0;
     // Citirea datelor de intrare = expresia logica
 
@@ -103,6 +116,7 @@ int main() {
 
     p = strtok(buffer_aux, delim);
     while (p != NULL) {
+      
       strcpy(terms[no_terms], p);
       no_terms++;
       p = strtok(NULL, delim);
@@ -112,7 +126,8 @@ int main() {
     // Realocam memoria pt matricea index
     for (int i = 0; i < no_docs; i++) {
       index[i] = realloc(index[i], no_terms * sizeof(bool *));
-      memset(index[i], 0, sizeof(index[i]));
+      int array_size = sizeof(index[i])/sizeof(index[i][0]);
+      memset(index[i], 0, array_size);
     }
 
     // Completam matricea de tipul inverted index:
@@ -143,43 +158,56 @@ int main() {
     // In buffer: expresia logica data; despartite de spatii
     char sol[MAX_DOCS][MAX_LEN];
     bool NOT, result;
-    int no_brackets, k, j,
-        i_buff; // indicii documentelor continute docs[sol[k]] = numele
+    int no_brackets, k, j;
+    unsigned int i_buff; // indicii documentelor continute docs[sol[k]] = numele
                 // documentului
-    no_brackets = k = j = i_buff = 0;
+    k = 0;
     NOT = 0;
-
+  
     for (int i = 0; i < no_docs; i++) {
-        result = 0;
+        j = i_buff = no_brackets = 0;
+        result = index[i][0];
       // i = doc; j = term
-      while (abs(i_buff) < strlen(buffer)) {
-
-        if (buffer[(i_buff++)] == '!') {
+      while (i_buff < strlen(buffer)) {
+        
+        if (buffer[i_buff] == '!') {
           NOT = 1;
-        }
-
-        if (buffer[(i_buff++)] == '(') {
-                    result = solve_brackets(buffer, &no_brackets, &i_buff, &result, &NOT, i, &j, terms, index);
-        }
-
-        while ((buffer[i_buff] > 'a' && buffer[i_buff] < 'z') ||
-               (buffer[i_buff] > 'A' && buffer[i_buff] < 'Z')) {
           i_buff++;
         }
 
-        if (buffer[i_buff++] == ' ') {
+        if (buffer[(i_buff)] == '(') {
+          i_buff++;
+          // intra pe pozitia de litera
+          result = solve_brackets(buffer, &no_brackets, &i_buff, &result, &NOT, i, &j, terms, index);
+        } // cand iese de aici, este pe pozitia primei litere dintr un termen
+
+
+        if (buffer[i_buff] == ' ') {
+          i_buff++;
+        }
+
+        if (is_letter(buffer[i_buff])) {
           j++;
+        }
+
+        while (is_letter(buffer[i_buff])) {
+          i_buff++;
+        }
+
+        if (buffer[i_buff] == ' ') {
+          i_buff++;
         }
 
         // Logical interpretation
         if (buffer[i_buff] == '&' && buffer[i_buff + 1] == '&') {
           result &= index[i][j];
+          i_buff += 2;
         }
 
         if (buffer[i_buff] == '|' && buffer[i_buff + 1] == '|') {
           result |= index[i][j];
+          i_buff += 2;
         }
-        i_buff += 2;
       }
       if (result == 1) {
           strcpy(sol[k++], docs[i]);
@@ -187,11 +215,16 @@ int main() {
     }
 
     // Afisarea rezultatelor
-    printf("Query match in:");
-    for (int i = 0; i < k; i++) {
-        printf(" %s", sol[i]);
+    if(k) {
+      printf("Query match in:");
+      for (int i = 0; i < k; i++) {
+          printf(" %s", sol[i]);
+      }
+      printf("\n");
+    } else {
+      system()
+      printf("No match found\n");
     }
-    printf("\n");
 
     printf("Enter your query below or type \"q\" to quit\n");
   }
